08d58d3d6a7edb2a4f2bb296610af98b183f3dac
Gustavo M <gustavomartini@protonmail.com>
2020-06-29 17:58:03 -0300

    new files

---
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,9 @@
+console.log(7 / 0); //retorna Infinity
+console.log("10" / 2); //divide
+console.log('3' + 2); //concatena
+console.log('3' - 2); //subtrai
+console.log("Show!" * 2); //NaN = not a number
+console.log(0.1 + 0.7); //imprecisão
+//console.log(10.toSring());
+console.log((10.345).toFixed(2));
+console.log((10).toFixed(2));
---
+++ exercicios-js/fundamentos/strings.js
@@ -0,0 +1,21 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4));
+console.log(escola.charAt(5)); //n gera erro, retorna vazio
+console.log(escola.charCodeAt(3)); //cod tabela Unicode
+console.log(escola.indexOf('3'));
+
+console.log(escola.substring(1));
+console.log(escola.substring(0, 3));
+
+console.log('Escola '.concat(escola).concat("!"));
+console.log(escola.replace(3, 'e'));
+
+console.log('Ana,Marta,Pedro'.split(','));
+
+
+
+
+
+
+
