f5b6fae9de371fb5d870b80e9daaff8ba87a7f39
Gustavo M <gustavomartini@protonmail.com>
2020-07-13 21:57:24 -0300

    first commit

---
+++ exercicios-js/fundamentos/hoisting.js
@@ -0,0 +1,18 @@
+console.log('a =', a);
+var a = 2
+console.log('a =', a);
+
+/** 
+ * ocorre o içamento da variável pro topo do escopo
+ * mesma coisa que isso:
+ * 
+ * var a
+ * console.log('a =', a);
+ * a = 2
+ * console.log('a =', a);
+ */
+
+ console.log('b =', b);
+ let b = 2
+ console.log('b =', b);
+// não funciona com let
